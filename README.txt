c771ff199ec4c532a3e149ef4c562f11affe0ee0
Davide Moro <davide.moro@gmail.com>
2014-08-15 11:05:40 +0200

    added webapp

--- README.txt
+++ README.txt
@@ -1,1 +1,14 @@
 p_started README
+
+
+
+[app:main]
+...
+
+PRODUCTION = false
+
+
+OR
+
+PRODUCTION env var
+
--- p_started/__init__.py
+++ p_started/__init__.py
@@ -1,12 +1,24 @@
+import os
 from pyramid.config import Configurator
+from pyramid.settings import asbool
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+    import pdb; pdb.set_trace()
+    production_config = asbool(settings.get('PRODUCTION', 'false'))
+    production = os.environ.get('PRODUCTION', production_config)
     config.include('pyramid_chameleon')
-    config.add_static_view('static', 'static', cache_max_age=3600)
+
+    if production:
+        config.add_static_view('static', 'static', cache_max_age=3600)
+    else:
+        config.add_static_view('static', 'static', cache_max_age=3600)
+        config.add_static_view('scripts', 'webapp/app/scripts', cache_max_age=3600)
+        config.add_static_view('styles', 'webapp/app/styles', cache_max_age=3600)
+        config.add_static_view('images', 'webapp/app/images', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
     return config.make_wsgi_app()
